8030fd4f336c095d4ecc31c9beb8b3e0d6cd7415
Sma Cohen <cohen.samuel@wustl.edu>
2020-12-20 10:50:08 -0500

    final commit with last edits for readability

--- transcript_from_youtube.py
+++ transcript_from_youtube.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6a37adbc11fc64906f156bc6b30606532cf1a39b4bb11b1ef92b685ac911cf93
-size 20493
+oid sha256:3f824fb1bff4023ddf795c1ceee20fdf5d6440f6318d958f09d51caf43d33e0d
+size 16168
